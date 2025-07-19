bb316fd10453b0d4fb74e2b85356783633ba0597
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-20 00:41:08 +0300

    Update get_func.py

--- safe_repo/core/get_func.py
+++ safe_repo/core/get_func.py
@@ -463,7 +463,7 @@ async def settings_command(event):
         [Button.inline("Remove Words", b'delete'), Button.inline("Reset", b'reset')],
         [Button.inline("Login", b'addsession'), Button.inline("Logout", b'logout')],
         [Button.inline("Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.url("Report Errors", "https://t.me/safe_repo")]
+        [Button.url("Report Errors", "https://t.me/xspes")]
     ]
     
     await gf.send_message(
