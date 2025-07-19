543eda91af93b247e99cac84d57be0a748b57db7
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-20 00:59:38 +0300

    Update plans.py

--- safe_repo/modules/plans.py
+++ safe_repo/modules/plans.py
@@ -39,10 +39,10 @@ async def myplan(client, message):
     data = await plans_db.check_premium(user_id)  
     if data and data.get("expire_date"):
         expiry = data.get("expire_date")
-        expiry_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata"))
-        expiry_str_in_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")            
+        expiry_ist = expiry.astimezone(pytz.timezone("Africa/Nairobi"))
+        expiry_str_in_ist = expiry.astimezone(pytz.timezone("Africa/Nairobi")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")            
         
-        current_time = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        current_time = datetime.datetime.now(pytz.timezone("Africa/Nairobi"))
         time_left = expiry_ist - current_time
             
         
@@ -66,10 +66,10 @@ async def get_premium(client, message):
         data = await plans_db.check_premium(user_id)  
         if data and data.get("expire_date"):
             expiry = data.get("expire_date") 
-            expiry_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata"))
-            expiry_str_in_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")            
+            expiry_ist = expiry.astimezone(pytz.timezone("Africa/Nairobi"))
+            expiry_str_in_ist = expiry.astimezone(pytz.timezone("Africa/Nairobi")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")            
             
-            current_time = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+            current_time = datetime.datetime.now(pytz.timezone("Africa/Nairobi"))
             time_left = expiry_ist - current_time
             
             
@@ -89,7 +89,7 @@ async def get_premium(client, message):
 @app.on_message(filters.command("add") & filters.user(OWNER_ID))
 async def give_premium_cmd_handler(client, message):
     if len(message.command) == 4:
-        time_zone = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        time_zone = datetime.datetime.now(pytz.timezone("Africa/Nairobi"))
         current_time = time_zone.strftime("%d-%m-%Y\n⏱️ ᴊᴏɪɴɪɴɢ ᴛɪᴍᴇ : %I:%M:%S %p") 
         user_id = int(message.command[1])
         user = await client.get_users(user_id)
@@ -100,7 +100,7 @@ async def give_premium_cmd_handler(client, message):
             await plans_db.add_premium(user_id, expiry_time)  
             data = await plans_db.check_premium(user_id)
             expiry = data.get("expire_date")   
-            expiry_str_in_ist = expiry.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")         
+            expiry_str_in_ist = expiry.astimezone(pytz.timezone("Africa/Nairobi")).strftime("%d-%m-%Y\n⏱️ ᴇxᴘɪʀʏ ᴛɪᴍᴇ : %I:%M:%S %p")         
             await message.reply_text(f"ᴘʀᴇᴍɪᴜᴍ ᴀᴅᴅᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ✅\n\n👤 ᴜꜱᴇʀ : {user.mention}\n⚡ ᴜꜱᴇʀ ɪᴅ : <code>{user_id}</code>\n⏰ ᴘʀᴇᴍɪᴜᴍ ᴀᴄᴄᴇꜱꜱ : <code>{time}</code>\n\n⏳ ᴊᴏɪɴɪɴɢ ᴅᴀᴛᴇ : {current_time}\n\n⌛️ ᴇxᴘɪʀʏ ᴅᴀᴛᴇ : {expiry_str_in_ist} \n\n__**Powered by safe_repo__**", disable_web_page_preview=True)
             await client.send_message(
                 chat_id=user_id,
